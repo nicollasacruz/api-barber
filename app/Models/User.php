0f42eacb4440470c6724eac4cabec570ffab6554
Nicollas Cruz <nicollasacruz@gmail.com>
2024-03-11 14:30:15 +0000

    Initial ajusts

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -49,4 +50,14 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return in_array($value, $this->role);
     }
+
+    // public function clientSchedules(): HasMany
+    // {
+    //     return $this->hasMany(Schedule::class, 'client_id', 'id');
+    // }
+
+    // public function barberSchedules(): HasMany
+    // {
+    //     return $this->hasMany(Schedule::class, 'barber_id', 'id');
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
+        
         return $request->user();
     });
 });
