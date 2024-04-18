f208da05cd5fff8093188f687516842ec34b210a
Nicollas Cruz <nicollasacruz@gmail.com>
2024-04-18 22:20:27 +0100

    Filament install

--- volumes/api-barber/vite.config.js
+++ volumes/api-barber/vite.config.js
@@ -8,4 +8,19 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+        },
+    },
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
+    watch: {
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
